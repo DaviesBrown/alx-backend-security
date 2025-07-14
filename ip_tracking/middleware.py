cd8e7cf8e18ee2458ade7a12412b9088a4448b01
Nwosu David <nwosudavid77@gmail.com>
2025-07-14 19:35:48 +0100

    Create middleware for logging ip address, timestamp and path from requests

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,19 @@
+from .models import RequestLog
+
+class IPLoggingMiddleware:
+    """
+    Middleware to log the IP address of each request.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip_address = request.META.get('REMOTE_ADDR', 'unknown')
+        path = request.path
+        print(f"Request from IP: {ip_address}")
+        print(f"Request path: {path}")
+        RequestLog.objects.create(ip_address=ip_address, path=path)
+
+        # Call the next middleware or view
+        response = self.get_response(request)
+        return response
